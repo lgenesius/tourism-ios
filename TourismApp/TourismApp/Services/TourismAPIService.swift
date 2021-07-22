2b991f83ebd85dfed2db766406dfbfbe74ca747a
Luis Genesius <luis.huang321@gmail.com>
2021-07-22 10:43:25 +0700

    set protocol and error enum in tourism api service file

--- TourismApp/TourismApp/Services/TourismAPIService.swift
+++ TourismApp/TourismApp/Services/TourismAPIService.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+protocol TourismAPIService {
+    
+    func fetchAll(completion: @escaping (Result<TourismAPIResponse, APIError>) -> ())
+}
+
+enum APIError: Error, CustomNSError {
+    case apiError
+    case invalidEndpoint
+    case invalidResponse
+    case noData
+    case serializationError
+    
+    var localizedDescription: String {
+        switch self {
+            case .apiError:
+                return "Failed to fetch data"
+            case .invalidEndpoint:
+                return "Invalid endpoint"
+            case .invalidResponse:
+                return "Invalid response"
+            case .noData:
+                return "No data"
+            case .serializationError:
+                return "Failed to decode data"
+        }
+    }
+    
+    var errorUserInfo: [String : Any] {
+        [NSLocalizedDescriptionKey : localizedDescription]
+    }
+}
